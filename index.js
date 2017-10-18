27b7cb79803c54f0404d4b53df5dc34473253691
Christine QE <christine.q.ernst@gmail.com>
2017-10-18 07:02:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,12 +24,10 @@ function logWhisper(string) {
   })
 })*/
 function sayHiToGrandma(string) {
-  var uppercase = string.toUpperCase()
-  var lowercase = string.toLowerCase()
 
-  if string === lowercase {
+  if (string === string.toLowerCase()) {
     return "I can\'t hear you!"
-  } else if string === uppercase {
+  } else if (string === string.toUpperCase()) {
     return "YES INDEED!"
   } else if string === "I love you, Grandma." {
     return "I love you, too."
